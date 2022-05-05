37352209b248ffc1a8133819a028e039c33f5cb7
Zach Strout <zachstrout@gmail.com>
2022-05-05 02:15:11 -0500

    Move header toggle function into class
    The header toggle button did not require too much from
    other objects, so I just pulled it in to simplify everything.

--- src/HeaderDOMManager.js
+++ src/HeaderDOMManager.js
@@ -10,7 +10,7 @@ class HeaderDOMManager {
     this.#headerElement = headerElement;
     this.#headerPlaceholder = document.createDocumentFragment();
     this.make()
-
+    this.menuButton.addEventListener("click", this.toggleProjectMenu.bind(this));
   }
   make() {
     this.#headerPlaceholder.appendChild(document.createElement("div"));
@@ -20,13 +20,16 @@ class HeaderDOMManager {
     this.#headerPlaceholder.lastChild.lastChild.textContent = "chevron_right";
     this.#headerElement.appendChild(this.#headerPlaceholder);
   }
+
+  toggleProjectMenu() {
+    const headerButton = this.menuButton.textContent;
+    this.menuButton.textContent = headerButton === "chevron_right" ? "chevron_left" : "chevron_right";
+    const gridWidth = this.#headerElement.parentElement.style.gridTemplateColumns;
+    this.#headerElement.parentElement.style.gridTemplateColumns = (gridWidth === "") ? "400px 1fr" : "";
+  }
   get menuButton(){
     return document.getElementById("projectMenuButton");
-
   }
-
-
-
 }
 
 export default HeaderDOMManager;
